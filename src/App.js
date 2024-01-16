4dd07fe7cde5a462f9739178e836f795ea2893bc
root <rakeshkarthikgrk@gmail.com>
2024-01-16 14:57:20 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css'
+
+import BrowserContainer from './components/BrowserContainer'
+
+// These are the list used in the application. You can move them to any component needed.
+
+// Replace your code here
+const App = () => <BrowserContainer />
+
+export default App
---
+++ src/components/BrowserElements/index.js
@@ -0,0 +1,34 @@
+import './index.css'
+
+const BrowserElement = props => {
+  const {HistoryListItem, deletedItem, uniqueValue} = props
+  const {timeAccessed, logoUrl, title, domainUrl} = HistoryListItem
+
+  const deleteItem = () => {
+    deletedItem(uniqueValue)
+  }
+  return (
+    <li className="historyItem">
+      <p className="time">{timeAccessed}</p>
+      <div className="itemContainer">
+        <img src={logoUrl} alt="domain logo" className="domainLogo" />
+        <p className="title">{title}</p>
+        <p className="domainUrl">{domainUrl}</p>
+      </div>
+      <button
+        onClick={deleteItem}
+        type="button"
+        data-testis="delete"
+        className="button"
+      >
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/delete-img.png"
+          alt="delete"
+          className="deleteLogo"
+        />
+      </button>
+    </li>
+  )
+}
+
+export default BrowserElement
